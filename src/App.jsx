aaf913e652e4e1c5a2d25faf9b7cba25505f2621
KLmk1 <klimakarov@gmail.com>
2025-10-17 18:50:12 +0400

    menu page catalog add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ProductList from './components/ProductList/ProductList';
 import Cart from './components/Cart/Cart';
 import Modal from './components/Modal/Modal';
